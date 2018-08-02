bac7dda2171d25eb89df7b96c8bf88f6aab847eb
FirstPacificator <novik.ap@gmail.com>
2018-08-02 22:50:39 +0300

    Added description in several scripts

--- Scripts/AutoStart.lua
+++ Scripts/AutoStart.lua
@@ -1,23 +1,14 @@
 ----  Скрипт должен срабатывать при каждом старте Miranda, кроме первого!
 ---1. Ждем полной загрузки ядра Miranda
----2. Создаем функцию WriteTabSRMMSkin, в которой определяем значение ключа CaptionOffset в секции WindowFrame в файле скина *.tsk (TabSRMM) для определенного перечня скинов:
-----  
----2. Читаем в БД в секции PackInfo значение ключа Font (unicode), если Tahoma, то
----3. пишем в файл (но только если такой файл существует) текущего скина TabSRMM значение определенное функцией WriteTabSRMMSkin
----3. 
+---2. Создаем функцию WriteTabSRMMSkin, в которой определяем значение ключа CaptionOffset
+----  (ключ отвечает за расположение значка и текста в шапке диалога сообщей) в секции
+----  WindowFrame в файле скина *.tsk (TabSRMM) для нескольких перечней скинов.
+----  Определяем два перечня скинов: patternLong и patternShort.
+----  Определяем шрифты, которые будем поддерживать в сборке: Tahoma, Verdana и Segoe Print
+---2. Читаем в БД в секции PackInfo значение ключа Font (unicode), и согласно функции WriteTabSRMMSkin
+---3. пишем в файлы (только в существующие) *.tsk значения определенные этой функцией
+---4. 
 
----1. Проверяем существует ли сервис 'Miranda/System/ModulesLoaded' если нет, ждем 0,5 секунды и снова проверяем (и так ∞)...
----2. Если сервис существует, то проверяем в БД в секции FirstRun значение ключа Lua_FirstRun (byte), если 1 то брейк, если 0, то едем дальше...
----3. Читаем в БД в секции PackInfo значение ключа Skin (unicode), и импортируем в БД %miranda_path%\\UserSet\\Skins\\'значение ключа PackInfo\Skin'.ini, и едем дальше...
----4. Читаем в БД в секции PackInfo значение ключа Font (unicode), и импортируем в БД %miranda_path%\\UserSet\\Fonts\\'значение ключа PackInfo\Font'.ini, и едем дальше...
----5. Проверяем существует ли сервис 'SmileyAdd/Reload' если нет, то пропускаем строку '6.' и прыгаем на строку '7.', если существует, то едем дальше...
----6. Читаем в БД в секции PackInfo значение ключа Smileys (unicode), и импортируем в БД %miranda_path%\\UserSet\\Smileys\\'значение ключа PackInfo\Smileys'.ini, и едем дальше...
----7. Меняем в БД в секции CList значение ключа State (byte) на 2 (0 - контакт лист свернут, 2 - контакт лист развернут)
----8. Меняем в БД в секции FirstRun значение ключа AccManager (byte) на 0 (0 - при старте Miranda будет открыт диалог "Учетные записи", 1 - не будет)
----9. Меняем в БД в секции FirstRun значение ключа Lua_FirstRun (byte) на 1 (0 - скрипт FirstRun.lua еще не выполнялся, 1 - уже выполнялся)
---10. Меняем в БД в секции PackInfo значение ключа MirVer (unicode) на текущее значение MirVer (текущая версия Miranda)
---11. Удаляем в БД секцию PluginDisable (в этой секции хранятся настройки плагинов, которые должны или не должны быть загружены вместе с Miranda)
---12. Дергаем сервис 'Miranda/System/ModulesLoaded' (Перезагрузка)
 assert(m)
 local db = require('m_database')
 assert(m)
@@ -47,7 +38,7 @@ end
 
 WriteTabSRMMSkin(db.GetSetting(_, 'PackInfo', 'Skin'), db.GetSetting(_, 'PackInfo', 'Font'))
 
-if db.GetSetting(_, 'FirstRun', 'MirLua') then
+if db.GetSetting(_, 'FirstRun', 'Lua_FirstRun') then
   local path = m.Parse('%miranda_path%\\Profiles\\update.ini')
   local hFile = io.open(path, "r")
   if not hFile then
