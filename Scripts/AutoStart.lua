476ab2d2077a5ab66fa4f01ec7cf9a0ec34a8161
FirstPacificator <im.hotcoffee@gmail.com>
2018-08-03 16:32:34 +0300

    Small fix

--- Scripts/AutoStart.lua
+++ Scripts/AutoStart.lua
@@ -1,13 +1,14 @@
-----  Скрипт должен срабатывать при каждом старте Miranda, кроме первого!
----1. Ждем полной загрузки ядра Miranda
----2. Создаем функцию WriteTabSRMMSkin, в которой определяем значение ключа CaptionOffset
-----  (ключ отвечает за расположение значка и текста в шапке диалога сообщей) в секции
-----  WindowFrame в файле скина *.tsk (TabSRMM) для нескольких перечней скинов.
-----  Определяем два перечня скинов: patternLong и patternShort.
-----  Определяем шрифты, которые будем поддерживать в сборке: Tahoma, Verdana и Segoe Print
----2. Читаем в БД в секции PackInfo значение ключа Font (unicode), и согласно функции WriteTabSRMMSkin
----3. пишем в файлы (только в существующие) *.tsk значения определенные этой функцией
----4. 
+---  Скрипт должен срабатывать при каждом старте Miranda!
+--1. Ждем полной загрузки ядра Miranda
+--2. Создаем функцию WriteTabSRMMSkin, в которой определяем значение ключа CaptionOffset
+---  (ключ отвечает за расположение значка и текста в шапке диалога сообщей) в секции
+---  WindowFrame в файле скина *.tsk (TabSRMM) для нескольких перечней скинов.
+---  Определяем два перечня скинов: patternLong и patternShort.
+---  Определяем шрифты, которые будем поддерживать в сборке: printing и handwriting
+--2. Читаем в БД в секции PackInfo значение ключа Font (unicode)
+--3. Пишем во все файлы (только в существующие) *.tsk в секцию WindowFrame,
+---  ключ CaptionOffset, значение определенное функцией WriteTabSRMMSkin
+--4. 
 
 assert(m)
 local db = require('m_database')
@@ -16,8 +17,8 @@ local winapi = require('winapi')
 assert(m)
 
 function WriteTabSRMMSkin(skinName, fontName)
-  local patternLong = 'diplomatd,diplomatl,glamourd,glamourl,goldtimed,goldtimel,photooned,photoonel,surfaceb,surfacew'
-  local patternShort = 'graphited,graphitel,textolited,textolitel'
+  local patternLong = 'Diplomat_Dark,Diplomat_Light,Glamour_Dark,Glamour_Light,GoldTime_Dark,GoldTime_Light,PhotoOne_Dark,PhotoOne_Light,Surface_Black,Surface_White'
+  local patternShort = 'Graphite_Dark,Graphite_Light,Textolite_Dark,Textolite_Gray_Dark,Textolite_Light'
 
   local result = 0
 
@@ -43,14 +44,14 @@ if db.GetSetting(_, 'FirstRun', 'Lua_FirstRun') then
   local hFile = io.open(path, "r")
   if not hFile then
     if db.GetSetting(_, 'FirstRun', 'AccManager', 1) == 0 then
-      m.CallService("Protos/ShowAccountManager")
-      db.WriteSetting(_, 'FirstRun', 'AccManager', 1, 1)
+       m.CallService("Protos/ShowAccountManager")
+       db.WriteSetting(_, 'FirstRun', 'AccManager', 1, 1)
     end
   elseif db.GetSetting(_, 'PackInfo', 'MirVer') == m.Version then
     io.close(hFile)
     if db.GetSetting(_, 'FirstRun', 'AccManager', 1) == 0 then
-      m.CallService("Protos/ShowAccountManager")
-      db.WriteSetting(_, 'FirstRun', 'AccManager', 1, 1)
+       m.CallService("Protos/ShowAccountManager")
+       db.WriteSetting(_, 'FirstRun', 'AccManager', 1, 1)
     end
   else
     io.close(hFile)
