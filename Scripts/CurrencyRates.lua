5f6c0ffd8f2680b521f30e111037b1349d8fa0f4
aunsane <aunsane@gmail.com>
2018-08-12 22:52:50 +0300

    Fix CurrencyRates.lua

--- Scripts/CurrencyRates.lua
+++ Scripts/CurrencyRates.lua
@@ -3,11 +3,18 @@
 --2. Если сервис существует, то читаем в БД в секции FirstRun значение ключа Lua_CurrencyRates (byte), если 1 то брейк, если 0, то едем дальше...
 --3. Импортируем файл '%miranda_path%\\UserSet\\CurrencyRates\\CR.xml' через сервис 'CurrencyRates/Import'.
 --4. Меняем в БД в секции FirstRun значение ключа Lua_CurrencyRates (byte) на 1 (0 - скрипт 'CurrencyRates.lua' еще не выполнялся, 1 - уже выполнялся)
+assert(m)
 local db = require('m_database')
+assert(db)
 local winapi = require('winapi')
+assert(winapi)
 
-if db.GetSetting(_, 'FirstRun', 'Lua_CurrencyRates', 1) == 0 then
+if not m.ServiceExists('') then
+  return
+end
+
+if db.GetSetting(_, 'FirstRun', 'Lua_CurrencyRates', 0) == 0 then
   local mCurrencyRatesXmlPath = toansi(m.Parse('%miranda_path%\\UserSet\\CurrencyRates\\CR.xml'))
   m.CallService('CurrencyRates/Import', 0, mCurrencyRatesXmlPath)
-  db.WriteSetting(_, 'FirstRun', 'Lua_CurrencyRates', 1, 1)
+  db.WriteSetting(_, 'FirstRun', 'Lua_CurrencyRates', 1, db.DBVT_BYTE)
 end
