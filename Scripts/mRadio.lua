b3643219720cee5d850e55d8cf2318aeabbad38e
FirstPacificator <novik.ap@gmail.com>
2018-08-13 23:22:10 +0300

    ...

--- Scripts/mRadio.lua
+++ Scripts/mRadio.lua
@@ -1,4 +1,4 @@
----  Скрипт должен сработать только если сервис 'mRadio/Import' существует и в БД в секции FirstRun значение ключа Lua_mRadio (byte) = 0
+---  Скрипт сработает через 20 минут после старта миранды... Если сервис 'mRadio/Import' существует, и в БД в секции FirstRun значение ключа Lua_mRadio (byte) = 0
 --1. Проверяем существует ли сервис 'mRadio/Import' если нет, то брейк, если существует, то едем дальше...
 --2. Если сервис существует, то читаем в БД в секции FirstRun значение ключа Lua_mRadio (byte), если 1 то брейк, если 0, то едем дальше...
 --3. Импортируем файл '%miranda_path%\\UserSet\\mRadio\\CR.xml' через сервис 'mRadio/Import'.
@@ -9,14 +9,18 @@ local db = require('m_database')
 assert(db)
 local winapi = require('winapi')
 assert(winapi)
+schedule = require('m_schedule')
+assert(schedule)
 
 if not m.ServiceExists('mRadio/Import') then
   return
 end
 
-if db.GetSetting(_, 'FirstRun', 'Lua_mRadio', 0) == 0 then
-  local mRadioIniPath = toansi(m.Parse('%miranda_path%\\UserSet\\mRadio\\mRadio.ini'))
-  m.CallService('mRadio/Import', 0, mRadioIniPath)
-  db.WriteSetting(_, 'FirstRun', 'Lua_mRadio', 1, db.DBVT_BYTE)
-  db.WriteSetting(_, 'mRadio', 'Volume', 100, db.DBVT_BYTE)
-end
+schedule.At(os.time() + 2400).Do(function()
+  if db.GetSetting(_, 'FirstRun', 'Lua_mRadio', 0) == 0 then
+    local mRadioIniPath = toansi(m.Parse('%miranda_path%\\UserSet\\mRadio\\mRadio.ini'))
+    m.CallService('mRadio/Import', 0, mRadioIniPath)
+    db.WriteSetting(_, 'FirstRun', 'Lua_mRadio', 1, db.DBVT_BYTE)
+    db.WriteSetting(_, 'mRadio', 'Volume', 100, db.DBVT_BYTE)
+  end
+end)
