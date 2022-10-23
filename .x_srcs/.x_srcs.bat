3a70ccbe618ca27f914d87c4da8c579e629aacff
FirstPacificator <im.hotcoffee@gmail.com>
2022-10-23 17:22:09 +0300

    ...

--- .x_srcs/.x_srcs.bat
+++ .x_srcs/.x_srcs.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set url="https://miranda-ng.org/distr/packs/HotCoffee/.xx_srcs.7z"
-set dest=".xx_srcs.7z"
+set url="https://miranda-ng.org/distr/packs/HotCoffee/.x_srcs.7z"
+set dest=".x_srcs.7z"
 call "..\.tools\curl.exe" %url% --output %dest%
 call "..\.tools\7z.exe" x %dest% -aoa -o"%~dp0" -y
 del %dest%
